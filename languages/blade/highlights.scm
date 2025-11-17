8e4d817e309260c874a2f63b0813921e8003b86e
Baptiste Bouillot <baptiste@ultraviolettes.fr>
2025-11-17 12:36:39 +0100

    Fix grammar versions: use v0.12.3 for blade, v0.24.2 for php
    - tree-sitter-blade v0.14.0 doesn't exist, using v0.12.3 (latest)
    - Updated tree-sitter-php to v0.24.2 (latest)
    - Re-downloaded query files from correct versions
    - Added /grammars/ to .gitignore (build artifact)
    
    Fixes: 'failed to compile grammar blade' error in Zed

--- languages/blade/highlights.scm
+++ languages/blade/highlights.scm
@@ -1,1 +1,18 @@
-404: Not Found
+; inherits: html
+
+[
+  (directive)
+  (directive_start)
+  (directive_end)
+] @tag
+
+[
+  (php_tag)
+  (php_end_tag)
+  "{{"
+  "}}"
+  "{!!"
+  "!!}"
+  "("
+  ")"
+] @punctuation.bracket
